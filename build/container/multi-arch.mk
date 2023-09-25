6eb2b08b5da25a3ffc7678c801eaa3615966731f
mao.chen <mao.chen@xdxct.com>
2023-09-25 17:26:48 +0800

    build: with xdxsmi basic images
    Signed-off-by: mao.chen <mao.chen@xdxct.com>

--- build/container/multi-arch.mk
+++ build/container/multi-arch.mk
@@ -19,8 +19,8 @@ DOCKER_BUILD_PLATFORM_OPTIONS = --platform=linux/amd64,linux/arm64
 # We only have x86_64 packages for centos7
 build-centos7: DOCKER_BUILD_PLATFORM_OPTIONS = --platform=linux/amd64
 
-# We only generate amd64 image for ubuntu18.04
-build-ubuntu18.04: DOCKER_BUILD_PLATFORM_OPTIONS = --platform=linux/amd64
+# We only generate amd64 image for ubuntu20.04
+build-ubuntu20.04: DOCKER_BUILD_PLATFORM_OPTIONS = --platform=linux/amd64
 
 # We only generate a single image for packaging targets
 build-packaging: DOCKER_BUILD_PLATFORM_OPTIONS = --platform=linux/amd64
